f9f6f72f826878cd15517e8f24e3a5380045d889
stefin <stefinracho@gmail.com>
2024-04-23 17:03:50 -0700

    feat: add routes to website

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
-import { Button } from "./components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
   return (
-    <>
-      <h1 className="text-6xl mb-6">Digital Dragons Business Communications App</h1>
-      <Button>This is a Shadcn Button</Button>
-
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>This is a Shadcn Card</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
