829c6280e560652e7d99c5f344bf5130748f91f8
Gabriel N <gabriejn@uci.edu>
2024-04-24 17:28:43 -0700

    Gabriel SideBar Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import LeftSideBar from "./pages/LeftSideBar";
+import AddChannel from "./pages/AddChannel";
+import SettingsBar from "./pages/SettingsBar";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/LSB" element={<LeftSideBar />} />
+      <Route path="/Add" element={<AddChannel />} />
+      <Route path="/Bar" element={<SettingsBar />} />
+
     </Routes>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import LeftSideBar from "./pages/LeftSideBar";
+import AddChannel from "./pages/AddChannel.tsx";
+import SettingsBar from "./pages/SettingsBar.tsx";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <div>
+  {/*  <div className=" bg-gray-100 max-w-sm min-h-screen ">*/}
+
+      </div>
+
     </BrowserRouter>
   </React.StrictMode>,
 );
