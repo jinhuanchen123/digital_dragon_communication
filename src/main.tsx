27739a1f37f830a478cace26382ee4676e888bdf
stefin <stefinracho@gmail.com>
2024-04-26 17:59:11 -0700

    feat: add topbar with channel actions

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import LeftSideBar from "./pages/LeftSideBar";
-import AddChannel from "./pages/AddChannel.tsx";
-import SettingsBar from "./pages/SettingsBar.tsx";
-import ButtonTemplate from "./pages/ButtonTemplate.tsx";
-
-
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <div>
-{/* <div className=" bg-gray-100 max-w-sm min-h-screen ">
-  <LeftSideBar />
-  <LeftSideBar />
-  <LeftSideBar />
-  <AddChannel />
-  <SettingsBar />
-  <ButtonTemplate />
-</div>      */}</div>
-
-
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,10 @@
+import TopBar from "@/components/TopBar";
+
 export default function HomePage() {
-  return <h1>This is the Home page</h1>;
+  return (
+    <>
+      <h1>This is the Home page</h1>
+      <TopBar />
+    </>
+  );
 }
