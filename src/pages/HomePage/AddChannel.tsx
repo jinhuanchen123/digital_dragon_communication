7a3aed2aaa32d387abcf537b7b44215ad09ab148
jinhuanchen123 <150759871+jinhuanchen123@users.noreply.github.com>
2024-04-29 15:00:08 -0700

    Add files via upload

---
+++ src/pages/HomePage/AddChannel.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+export default function AddChannel() {
+    return <div >
+    <button className="flex mx-auto p-2 bg-purple-500 m-5">Add Channel</button>
+  </div>;
+  }
+  
---
+++ src/pages/SettingsPage/LogOut.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Setting.css'; // Correct import statement
+import RightSidebar from './RightSidebar';
+import { Link } from 'react-router-dom'; 
+
+function LogOut() {
+  return (
+    <RightSidebar/>  
+  );
+}
+
+export default LogOut;
