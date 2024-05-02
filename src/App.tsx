aee4e8551e1c06f13e957bb72e40c9dc55fd96cb
Daniel-Galvezcohort238 <daniel.galvez.cohort238@gmail.com>
2024-05-02 16:10:35 -0700

    added message history, moved message history and left side bar together

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,6 @@ export default function App() {
 
 
     </Routes>
-    <MessageHistory />
     </>
   );
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,22 @@
 import TopBar from "@/components/ui/TopBar";
+import AddChannel from "./AddChannel";
+import MessageHistory from './MessageHistory/MessageHistory'
+import './home-page.css'
+
+
 
 export default function HomePage() {
   return (
     <>
-      <h1>This is the Home page</h1>
-      <TopBar />
+    <h1>This is the Home page</h1>
+    <div id="home-page">
+      <AddChannel />
+      <div id="middle-components">
+        <TopBar />
+        <MessageHistory />
+      </div>
+    </div>
+    
     </>
   );
 }
