2105ef028c4587b5716f005b07e3aaed8704dc96
stefin <stefinracho@gmail.com>
2024-05-03 15:00:00 -0700

    fix: convert login page to module.css

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, FormEvent, useState } from "react";
-import "./LoginPage.css";
+import style from "./LoginPage.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { auth, googleProvider } from "../../firebase.ts";
@@ -84,115 +84,130 @@ export default function LoginPage() {
   }
 
   return (
-    <div className={`container ${isActive ? "active" : ""}`} id="container">
-      {/* Sign Up Section */}
-      <div className="form-container sign-up">
-        <form onSubmit={signUp}>
-          <h1 className="form-heading">Create Account</h1>
+    <div className={style.fullWrapper}>
+      <div
+        className={`${style.container} ${isActive ? style.active : ""}`}
+        id="container"
+      >
+        {/* Sign Up Section */}
+        <div className={`${style.formContainer} ${style.signUp}`}>
+          <form onSubmit={signUp}>
+            <h1 className={style.formHeading}>Create Account</h1>
 
-          {/* Social Icons */}
-          <div className="social-icons">
-            <button onClick={signInWithGoogle} className="icon">
-              {<FontAwesomeIcon icon={faGoogle} />}
-            </button>
-          </div>
+            {/* Social Icons */}
+            <div className={style.socialIcons}>
+              <button onClick={signInWithGoogle} className={style.icon}>
+                {<FontAwesomeIcon icon={faGoogle} />}
+              </button>
+            </div>
 
-          <span>or use your email for registration</span>
+            <span>or use your email for registration</span>
 
-          {/* Input Fields */}
-          <input
-            name="signUpName"
-            type="text"
-            onChange={handleChange}
-            value={formData.signUpName}
-            minLength={3}
-            placeholder="Name"
-            required
-          />
-          <input
-            name="signUpEmail"
-            type="email"
-            onChange={handleChange}
-            value={formData.signUpEmail}
-            placeholder="Email"
-            required
-          />
-          <input
-            name="signUpPassword"
-            type="password"
-            onChange={handleChange}
-            value={formData.signUpPassword}
-            minLength={6}
-            placeholder="Password"
-            required
-          />
+            {/* Input Fields */}
+            <input
+              name="signUpName"
+              type="text"
+              onChange={handleChange}
+              value={formData.signUpName}
+              minLength={3}
+              placeholder="Name"
+              required
+            />
+            <input
+              name="signUpEmail"
+              type="email"
+              onChange={handleChange}
+              value={formData.signUpEmail}
+              placeholder="Email"
+              required
+            />
+            <input
+              name="signUpPassword"
+              type="password"
+              onChange={handleChange}
+              value={formData.signUpPassword}
+              minLength={6}
+              placeholder="Password"
+              required
+            />
 
-          {/* Sign Up Button */}
-          <button>Sign Up</button>
-          {isSignUpError && <span className="error">Invalid Credentials</span>}
-        </form>
-      </div>
+            {/* Sign Up Button */}
+            <button>Sign Up</button>
+            {isSignUpError && (
+              <span className={style.error}>Invalid Credentials</span>
+            )}
+          </form>
+        </div>
 
-      {/* Sign In Section */}
-      <div className="form-container sign-in">
-        <form onSubmit={signIn}>
-          <h1 className="form-heading">Sign In</h1>
+        {/* Sign In Section */}
+        <div className={`${style.formContainer} ${style.signIn}`}>
+          <form onSubmit={signIn}>
+            <h1 className={style.formHeading}>Sign In</h1>
 
-          {/* Social Icons */}
-          <div className="social-icons">
-            <button onClick={signInWithGoogle} className="icon">
-              {<FontAwesomeIcon icon={faGoogle} />}
-            </button>
-          </div>
+            {/* Social Icons */}
+            <div className={style.socialIcons}>
+              <button onClick={signInWithGoogle} className={style.icon}>
+                {<FontAwesomeIcon icon={faGoogle} />}
+              </button>
+            </div>
 
-          <span>or use your email password</span>
+            <span>or use your email password</span>
 
-          {/* Input Fields */}
-          <input
-            name="signInEmail"
-            type="email"
-            onChange={handleChange}
-            value={formData.signInEmail}
-            placeholder="Email"
-            required
-          />
-          <input
-            name="signInPassword"
-            type="password"
-            onChange={handleChange}
-            value={formData.signInPassword}
-            placeholder="Password"
-            required
-          />
+            {/* Input Fields */}
+            <input
+              name="signInEmail"
+              type="email"
+              onChange={handleChange}
+              value={formData.signInEmail}
+              placeholder="Email"
+              required
+            />
+            <input
+              name="signInPassword"
+              type="password"
+              onChange={handleChange}
+              value={formData.signInPassword}
+              placeholder="Password"
+              required
+            />
 
-          {/* Forgot Password Link */}
-          <a href="#">Forgot Your Password?</a>
+            {/* Forgot Password Link */}
+            <a href="#">Forgot Your Password?</a>
 
-          {/* Sign In Button */}
-          <button>Sign In</button>
-          {isSignInError && <span className="error">Invalid Credentials</span>}
-        </form>
-      </div>
+            {/* Sign In Button */}
+            <button>Sign In</button>
+            {isSignInError && (
+              <span className={style.error}>Invalid Credentials</span>
+            )}
+          </form>
+        </div>
 
-      {/* Toggle Section */}
-      <div className="toggle-container">
-        <div className="toggle">
-          {/* <div className="toggle-panel toggle-left"> */}
-          <div className="toggle-panel toggle-left" onClick={showLogin}>
-            <h1>Welcome Back!</h1>
-            <p>Enter your personal details to use all site features</p>
-            <button className="hidden" id="login">
-              Sign In
-            </button>
-          </div>
+        {/* Toggle Section */}
+        <div className={style.toggleContainer}>
+          <div className={style.toggle}>
+            {/* <div className="toggle-panel toggle-left"> */}
+            <div
+              className={`${style.togglePanel} ${style.toggleLeft}`}
+              onClick={showLogin}
+            >
+              <h1>Welcome Back!</h1>
+              <p>Enter your personal details to use all site features</p>
+              <button className={style.hidden} id="login">
+                Sign In
+              </button>
+            </div>
 
-          {/* <div className="toggle-panel toggle-right"> */}
-          <div className="toggle-panel toggle-right" onClick={showSignup}>
-            <h1>Hello, Friend!</h1>
-            <p>Register with your personal details to use all site features</p>
-            <button className="hidden" id="register">
-              Sign Up
-            </button>
+            {/* <div className="toggle-panel toggle-right"> */}
+            <div
+              className={`${style.togglePanel} ${style.toggleRight}`}
+              onClick={showSignup}
+            >
+              <h1>Hello, Friend!</h1>
+              <p>Register with your personal details to use all site features</p>
+              <button className={style.Hidden} id="register">
+                Sign Up
+              </button>
+            </div>
           </div>
         </div>
       </div>
